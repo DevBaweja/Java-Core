5243aec99a3ec17a3586186d401b678f2a17ae4d
devbaweja2512 <53691664+devbaweja2512@users.noreply.github.com>
2019-08-18 15:16:03 +0530

    Add files via upload

---
+++ JavaFunctions/src/Sorting/InsertionSortFunction.java
@@ -0,0 +1,10 @@
+package Sorting;
+123
+public class InsertionSortFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaFunctions/src/Sorting/SelectionSortFunction.java
@@ -0,0 +1,10 @@
+package Sorting;
+123
+public class SelectionSortFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
